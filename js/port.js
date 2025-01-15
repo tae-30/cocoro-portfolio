8aad19c0782e5d68c45512c72e36e04895b6c5fa
tae-30 <yuut0302@icloud.com>
2025-01-16 01:36:17 +0900

    menuトランジション作成

--- js/port.js
+++ js/port.js
@@ -1,5 +1,5 @@
 // Menuボタンのクリックイベントを設定
 document.querySelector('.MenuIcon').addEventListener('click', function () {
     const menu = document.querySelector('.menu');
-    menu.style.display = menu.style.display === 'none' ? 'block' : 'none';
+    menu.classList.toggle('open');
 });
