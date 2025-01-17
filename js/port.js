82fbc3c0e1ee92786055f7532f86d1e45688482b
tae-30 <yuut0302@icloud.com>
2025-01-17 22:16:56 +0900

    transformを消去しバグを修正

--- js/port.js
+++ js/port.js
@@ -1,11 +1,11 @@
-// Menuボタンのクリックイベントを設定
-document.querySelector('.MenuIcon').addEventListener('click', function () {
-    const menu = document.querySelector('.menu');
-    menu.classList.toggle('open');
-});
-
-
 document.addEventListener("DOMContentLoaded", () => {
+    // Menuボタンのクリックイベント
+    document.querySelector('.MenuIcon').addEventListener('click', function () {
+        const menu = document.querySelector('.menu');
+        menu.classList.toggle('open');
+    });
+
+    // TopTitleのアニメーション
     const topTitle = document.querySelector(".TopTitle");
     topTitle.classList.add("visible");
 });
