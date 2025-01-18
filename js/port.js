88103e0c67744a601a2d5cbbe785c105b798c53b
tae-30 <yuut0302@icloud.com>
2025-01-18 21:05:52 +0900

    スクロールアニメーション実装

--- js/port.js
+++ js/port.js
@@ -8,4 +8,22 @@ document.addEventListener("DOMContentLoaded", () => {
     // TopTitleのアニメーション
     const topTitle = document.querySelector(".TopTitle");
     topTitle.classList.add("visible");
+
+    // メニュー内リンクを取得してスクロール処理を一括登録
+    const menuLinks = document.querySelectorAll('.menu a');
+    // 各要素に1つずつ処理を実行
+    menuLinks.forEach(link => {
+        link.addEventListener('click', function (event) {
+            event.preventDefault();
+
+            // 対象のセクションIDを取得してスクロール
+            // クリックされたリンクの属性値を取得しtargetIdに格納
+            const targetId = this.getAttribute('href');
+            gsap.to(window, { scrollTo: targetId, duration: 1.2 });
+
+            // メニューを閉じる
+            const menu = document.querySelector('.menu');
+            menu.classList.remove('open');
+        });
+    });
 });
